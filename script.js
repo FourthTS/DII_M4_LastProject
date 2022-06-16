9792e2b52bcf6ee958a249586be474249147f557
FourthTs <fourth4884@gmail.com>
2022-06-16 22:25:51 +0700

    add function in allainmepage

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var output = document.getElementById('contentArea')
+var output2 = document.getElementById('subContentArea')
 var id = 642110317
 
 let navBarArea = document.getElementById('navBarArea')
 let mainContentArea = document.getElementById('mainContentArea')
+let showResult = document.getElementById('showResultSearch')
 
 
 //------------------------------------------------------------ hidden page ---------------------------------------------------------------------//
@@ -10,6 +12,8 @@ function hiddenPage() {
     mainContentArea.innerHTML = ''
     output.innerHTML = ''
     navBarArea.innerHTML = ''
+    showResult.innerHTML = ''
+    output2.innerHTML = ''
 }
 
 //-------------------------------------------------- search function & search page---------------------------------------------------//
@@ -28,14 +32,15 @@ function searchInformation() {
             }).then((data => {
                 Search(data.data)
             }))
-})
+  })
+}
 
 function Search(dataList) {
     for (data of dataList) {
         addCard(data)
     }
 }
-}
+
 
 
 function addCard(movie) {
@@ -90,7 +95,6 @@ function addCard(movie) {
         }
     })
     output.appendChild(cardDiv)
-
 }
 //--------------------------------- Add information to My List in Database---------------------------------------------// 
 
@@ -117,6 +121,8 @@ function myListClick() {
     hiddenPage()
     onLoad()
     navBar()
+    showResult.innerHTML = `<div class="ms-3 mt-3"><p class="text-[1rem] md:text-[2rem] font-bold text-[#ffff]">ANIME IN 
+                            <span class="text-[#e74538]">MY LIST</span></p></div>`
 }
 
 function onLoad() {
@@ -132,19 +138,7 @@ function MyList(dataList) {
 
     for (data of dataList) {
         addCardMyList(data)
-        console.log(data.title)
-        checkInformations(data.title)
     }
-}
-//---------------------------------------------------------------- check information -----------------------------------------------//
-function checkInformations(dataList) {
-    fetch(`https://api.jikan.moe/v4/anime?q=${dataList}`)
-            .then((response) => {
-                console.log(response)
-            }).then((data => {
-                console.log('jiKan')
-                console.log(data)
-            }))
 }
 //----------------------------------------------------------------card my list ----------------------------------------------------//
 function addCardMyList(data) {
@@ -238,6 +232,7 @@ function showDetail(data) {
     img.classList.add('min-h-full')
     img.classList.add('h-full')
     img.classList.add('w-full')
+    img.classList.add('rounded-3')
 
     divImgCard.appendChild(img)
 
@@ -257,7 +252,14 @@ function showDetail(data) {
     button.classList.add('rounded-3')
 
     button.innerHTML="Remove from MyList"
-    button.setAttribute("onclick", "deleteMovie(data.id)")
+    button.addEventListener('click', function () {
+        var r = confirm(`delete ${data.title} On MyList`);
+        if (r == true) {
+            deleteMovie(data.id)
+            output.innerHTML = ''
+        }
+    })
+
     divButton.appendChild(button)
 
     divCard.appendChild(divImgCard)
@@ -299,6 +301,226 @@ function showDetail(data) {
     output.appendChild(divShowDetail)
 
 }
+// ----------------------------------- All Anime Page -----------------------------------------------------------------------------------//
+function allAnimePage() {
+    hiddenPage()
+    navBar()
+    mainContentArea.innerHTML = ` 
+  <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+    
+    
+    
+    <div class="carousel-inner w-[100%] h-[25rem]">
+      
+      <div class="carousel-item active bg-no-repeat"style="height:25rem; background-image: url(https://pbs.twimg.com/media/EPERK6XWoAAiraW.jpg); background-position: center; background-size: cover; ">
+        <div class="rox justify-content-center align-item-center position-absolute top-50 start-50 translate-middle">
+          <h5 class="text-center font-bold text-[2rem]">Top <span class="font-bold text-[2rem] text-[#e74538]">ANIME</span></h5>
+          <p  class="text-center text-[1rem]">Ranking fo ANIME </p>
+        </div>
+      </div>
+
+
+      <div class="carousel-item bg-no-repeat" style="height:25rem; background-image: url(https://www.online-station.net/wp-content/uploads/2021/12/02suzumenotojimari-00a.jpg); background-position: center; background-size: cover;>
+        <div class="d-flex justify-content-center align-item-center">
+          <div class="rox justify-content-center align-item-center position-absolute top-50 start-50 translate-middle">
+              <h5 class="text-center font-bold text-[2rem]">ANIME <span class="font-bold text-[2rem] text-[#e74538]">UPCOMING</span></h5>
+              <p class="text-center text-[1rem]">New Anime Coming Soon.</p>
+          </div>
+        </div>
+      
+
+
+      <div class="carousel-item bg-no-repeat" style="height:25rem; background-image: url(https://images8.alphacoders.com/667/thumb-1920-667873.png); background-position: center; background-size: cover;>
+        <div class="d-flex justify-content-center align-item-center">
+          <div class="rox justify-content-center align-item-center position-absolute top-50 start-50 translate-middle">
+              <h5 class="text-center font-bold text-[2rem]">ANIME <span class="font-bold text-[2rem] text-[#e74538]">AIRING</span></h5>
+              <p class="text-center text-[1rem]">All Anime Airing To Day</p>
+          </div>
+        </div>
+    
+    </div>
+
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>`
+  subAllAnimeContent ()
+}
+//--------------------------------------------------------------- All Anime page Sub content --------------------------------------------//
+function subAllAnimeContent (){
+  fetch(`https://api.jikan.moe/v4/anime`)
+            .then((response) => {
+                return response.json()
+            }).then((data => {
+                ListInformation(data.data)
+            }))
+}
+function  ListInformation(dataList) {
+    for (data of dataList) {
+        addCard2(data)
+    }
+}
+
+function addCard2(movie) {
+
+    let cardDiv = document.createElement('div')
+    cardDiv.classList.add("col-6")
+    cardDiv.classList.add("col-lg-2")
+    cardDiv.classList.add("col-md-3")
+    cardDiv.classList.add("col-sm-4")
+
+    let card = document.createElement('div')
+    card.classList.add("card")
+    card.classList.add("h-[13rem]")
+    card.classList.add("lg:h-[19rem]")
+    card.classList.add("md:h-[17rem]")
+    card.classList.add("sm:h-[15rem]")
+    card.classList.add("p-2")
+    card.classList.add("my-2")
+    card.classList.add("bg-no-repeat")
+    card.classList.add("bg-cover")
+    card.classList.add("bg-center")
+    card.setAttribute('style','background-image: url('+movie.images.jpg.large_image_url+')')
+
+    let cardClass = document.createElement('div')
+    cardClass.classList.add("description")
+    let head = document.createElement('p')
+    head.classList.add("card-title")
+    let name = movie.title
+    head.innerHTML = name
+    cardClass.appendChild(head)
+    
+
+    card.appendChild(cardClass)
+    cardDiv.appendChild(card)
+    cardDiv.addEventListener('click', function () {
+      hiddenPage()
+      navBar()
+      showDetail2(movie)
+        
+    })
+    output2.appendChild(cardDiv)
+}
+
+//----------------------------------------------------------------shoe detail 2----------------------------------------------------------//
+function showDetail2(data) {
+  let divShowDetail =document.createElement('div')
+  divShowDetail.classList.add('px-[12px]')
+  divShowDetail.classList.add('md:px-[40px]')
+  divShowDetail.classList.add('grid')
+  divShowDetail.classList.add('grid-cols-1')
+  divShowDetail.classList.add('justify-items-center')
+  divShowDetail.classList.add('gap-[70px]')
+  divShowDetail.classList.add('md:grid-cols-[250px_1fr]')
+  divShowDetail.classList.add('md:gap-[18px]')
+  divShowDetail.classList.add('my-3')
+
+  let divCard = document.createElement('div')
+  divCard.classList.add('max-h-[40vh]')
+
+  let divImgCard = document.createElement('div')
+  divImgCard.classList.add('w-full')
+  divImgCard.classList.add('min-w-full')
+  divImgCard.classList.add('h-full')
+
+  let img = document.createElement('img')
+  
+  img.src = data.images.jpg.large_image_url
+
+  img.classList.add('object-cover')
+  img.classList.add('min-w-full')
+  img.classList.add('min-h-full')
+  img.classList.add('h-full')
+  img.classList.add('w-full')
+  img.classList.add('rounded-3')
+
+  divImgCard.appendChild(img)
+
+  let divButton = document.createElement('div')
+  divButton.classList.add('my-2')
+
+
+  let button = document.createElement('button')
+  button.classList.add('text-sm')
+  button.classList.add('text-white')
+  button.classList.add('h-[37px]')
+  button.classList.add('w-full')
+  button.classList.add('flex')
+  button.classList.add('items-center')
+  button.classList.add('justify-center')
+  button.classList.add('bg-[#e74538]')
+  button.classList.add('rounded-3')
+
+  button.innerHTML="Add To MyList"
+  button.addEventListener('click', function () {
+    var r = confirm(`Add ${data.title} to MyList`);
+    if (r == true) {
+        allData = { id, movie:{
+            "url": `${data.url}`,
+            "image_url": `${data.images.jpg.image_url}`,
+            "title": `${data.title}`,
+            "synopsis":`${data.synopsis}`,
+            "type": `${data.type}`,
+            "episodes": `${data.episodes}`,
+            "score":`${data.score}`,
+            "rated": `${data.rating}`
+        } }
+        console.log(allData)
+        addtoMylistToDB(allData)
+
+    }
+  })
+
+  divButton.appendChild(button)
+
+  divCard.appendChild(divImgCard)
+  divCard.appendChild(divButton)
+
+  let divShowInformation = document.createElement('div')
+  divShowInformation.classList.add("grid")
+  divShowInformation.classList.add("auto-rows-min")
+
+  let head = document.createElement('p')
+  head.classList.add("my-4")
+  head.classList.add("text-[1.5rem]")
+  head.classList.add("md:text-[2rem]")
+  head.classList.add("font-bold")
+  head.classList.add("text-white")
+  head.innerHTML = data.title
+
+  let synopsis = document.createElement('p')
+  synopsis.classList.add("text-[1rem]")
+  synopsis.classList.add("md:text-[1.5rem]")
+  synopsis.classList.add("font-bold")
+  synopsis.classList.add("text-[#e74538]")
+  synopsis.innerHTML = "Synopsis"
+  
+  let synopsisDetile = document.createElement('p')
+  synopsisDetile.classList.add("text-white")
+  synopsisDetile.classList.add("opacity-80")
+  synopsisDetile.classList.add("text-[12px]")
+  synopsisDetile.classList.add("text-[11px]")
+  synopsisDetile.classList.add("md:text-[1rem")
+  synopsisDetile.innerHTML = data.synopsis
+
+  divShowInformation.appendChild(head)
+  divShowInformation.appendChild(synopsis)
+  divShowInformation.appendChild(synopsisDetile)
+
+  divShowDetail.appendChild(divCard)
+  divShowDetail.appendChild(divShowInformation)
+  output.appendChild(divShowDetail)
+}
 //---------------------------------------------------------------- navBar ---------------------------------------------------------------//
 function navBar() {
     let navBar = document.createElement('div')
@@ -315,7 +537,7 @@ function navBar() {
     divStart.classList.add("col-md-3")
     divStart.classList.add("pt-1.5")
     divStart.classList.add("sm:pt-0")
-    divStart.innerHTML = `<p class="text-1rem sm:text-[1.5rem] md:text-[2rem] font-semibold tracking-wide text-[#fff]">ANI<span class="text-[#e74538] cursor-pointer">MA</span></p>`
+    divStart.innerHTML = `<p class="text-1rem sm:text-[1.5rem] md:text-[2rem] ps-3 font-semibold tracking-wide text-[#fff]">ANI<span class="text-[#e74538] cursor-pointer">MA</span></p>`
     divStart.setAttribute("onclick", "homePageClick()")
 
     let divCenter = document.createElement('div')
@@ -343,7 +565,8 @@ function navBar() {
 
     let divAnime = document.createElement('div')
     divAnime.classList.add("py-[0.7rem]")
-    divAnime.classList.add("basis-20")
+    divAnime.classList.add("px-[0.7rem]")
+    divAnime.classList.add("base-basis-20")
     divAnime.classList.add("text-center")
     divAnime.classList.add("rounded-md")
     divAnime.classList.add("hover:bg-[#e74538]")
@@ -351,19 +574,10 @@ function navBar() {
     divAnime.classList.add("cursor-pointer")
 
     divAnime.id="animePage"
-    divAnime.innerText = 'ANIME'
+    divAnime.setAttribute("onclick", "allAnimePage()")
+    divAnime.innerText = 'ALL ANIME'
 
-    let divManga = document.createElement('div')
-    divManga.classList.add("py-[0.7rem]")
-    divManga.classList.add("basis-20")
-    divManga.classList.add("text-center")
-    divManga.classList.add("rounded-md")
-    divManga.classList.add("hover:bg-[#e74538]")
-    divManga.classList.add("hover:text-[#151f2e]")
-    divManga.classList.add("cursor-pointer")
 
-    divManga.id="mangaPage"
-    divManga.innerText = 'MANGA'
 
     let divMyList = document.createElement('div')
     divMyList.classList.add("py-[0.7rem]")
@@ -380,8 +594,6 @@ function navBar() {
 
     divCenter.appendChild(divHome)
     divCenter.appendChild(divAnime)
-    divCenter.appendChild(divManga)
-    divCenter.appendChild(divManga)
     divCenter.appendChild(divMyList)
 
     
@@ -417,8 +629,7 @@ function navBarOld() {
     </div>
     <div class="flex col-6 col-md-4 justify-evenly text-[0.7rem] xs:text-[0.8rem] sm:text-[0.9rem] md:text-[1rem]">
       <div class="py-[0.7rem] basis-20 text-center rounded-md hover:bg-[#e74538] hover:text-[#151f2e] cursor-pointer " id="homePage" onclick="homePageClick()">HOME</div>
-      <div class="py-[0.7rem] basis-20 text-center rounded-md hover:bg-[#e74538] hover:text-[#151f2e] cursor-pointer " id="animePage">ANIME</div>
-      <div class="py-[0.7rem] basis-20 text-center rounded-md hover:bg-[#e74538] hover:text-[#151f2e] cursor-pointer " id="mangaPage">MANGA</div>
+      <div class="py-[0.7rem] px-[0.7rem] base-basis-20 text-center rounded-md hover:bg-[#e74538] hover:text-[#151f2e] cursor-pointer " id="animePage" onclick="allAnimePage()">ALL ANIME</div>
       <div class="py-[0.7rem] basis-20 text-center rounded-md hover:bg-[#e74538] hover:text-[#151f2e] cursor-pointer " id="myListPage "onclick="myListClick()">MYLIST</div>
     </div>
     <div div class="flex col-0 col-md-4">
